6a1e390703aad3fd4401f1e0c3b4d9556992204d
StevenDRiggs <sfriggs21@gmail.com>
2021-11-29 14:36:32 -0700

    first run through componentDidMount; next: refactor

--- components/Meeting.tsx
+++ components/Meeting.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: MIT-0
  */
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, FlatList, Alert } from 'react-native';
 
 import { NativeFunction, getSDKEventEmitter, MobileSDKEvent, MeetingError } from '../utils/Bridge';
@@ -28,93 +28,67 @@ const Meeting = () => {
   const [ meetingTitle, setMeetingTitle ] = useState('')
   const [ screenShareTile, setScreenShareTile ] = useState(null)
 
-  // continue rewrite below this line
-  componentDidMount() {
-    /**
-     * Attendee Join and Leave handler
-     */
-    this.onAttendeesJoinSubscription = getSDKEventEmitter().addListener(MobileSDKEvent.OnAttendeesJoin, ({ attendeeId, externalUserId }) => {
+  const [ onAttendeesJoinSubscription, setOnAttendeesJoinSubscription ] = useState()
+  const [ onAttendeesLeaveSubscription, setOnAttendeesLeaveSubscription ] = useState()
+
+  useEffect(() => {
+    // Attendee Join and Leave handler
+
+    setOnAttendeesJoinSubscription(getSDKEventEmitter().addListener(MobileSDKEvent.OnAttendeesJoin, ({ attendeeId, externalUserId }) => {
       if (!(attendeeId in attendeeNameMap)) {
         // The externalUserId will be a format such as c19587e7#Alice
-        attendeeNameMap[attendeeId] = externalUserId.split("#")[1];
+        attendeeNameMap[attendeeId] = externalUserId.split("#")[1]
       }
-      this.setState((oldState) => ({ 
-        ...oldState, 
-        attendees: oldState.attendees.concat([attendeeId]) 
-      }));
-    });
 
-    this.onAttendeesLeaveSubscription = getSDKEventEmitter().addListener(MobileSDKEvent.OnAttendeesLeave, ({ attendeeId }) => {
-      this.setState((oldState) => ({ 
-        ...oldState,
-        attendees: oldState.attendees.filter((attendeeToCompare => attendeeId != attendeeToCompare)) 
-      }));
-    });
+      setAttendees(oldState => oldState.attendees.concat([attendeeId]))
+    }))
 
-    /**
-     * Attendee Mute & Unmute handler
-     */
-    this.onAttendeesMuteSubscription = getSDKEventEmitter().addListener(MobileSDKEvent.OnAttendeesMute, attendeeId => {
-      this.setState((oldState) => ({ 
-        ...oldState, 
-        mutedAttendee: oldState.mutedAttendee.concat([attendeeId]) 
-      }));
-    });
+    setOnAttendeesLeaveSubscription(getSDKEventEmitter().addListener(MobileSDKEvent.OnAttendeesLeave, ({ attendeeId }) => {
+      setAttendees(oldState => oldState.attendees.filter((attendeeToCompare => attendeeId != attendeeToCompare)))
+    }))
 
-    this.onAttendeesUnmuteSubscription = getSDKEventEmitter().addListener(MobileSDKEvent.OnAttendeesUnmute, attendeeId => {
-      this.setState((oldState) => ({ 
-        ...oldState,
-        mutedAttendee: oldState.mutedAttendee.filter((attendeeToCompare => attendeeId != attendeeToCompare)) 
-      }));
-    });
+    // Attendee Mute & Unmute handler
+    setOnAttendeesMuteSubscription(getSDKEventEmitter().addListener(MobileSDKEvent.OnAttendeesMute, attendeeId => {
+      setMutedAttendee(oldState => oldState.mutedAttendee.concat([attendeeId]))
+    }))
 
-    /**
-     * Video tile Add & Remove Handler
-     */
-    this.onAddVideoTileSubscription = getSDKEventEmitter().addListener(MobileSDKEvent.OnAddVideoTile, (tileState) => {
+    setOnAttendeesUnmuteSubscription(getSDKEventEmitter().addListener(MobileSDKEvent.OnAttendeesUnmute, attendeeId => {
+      setMutedAttendee(oldState => oldState.mutedAttendee.filter((attendeeToCompare => attendeeId != attendeeToCompare)))
+    }))
+
+    // Video tile Add & Remove Handler
+
+    setOnAddVideoTileSubscription(getSDKEventEmitter().addListener(MobileSDKEvent.OnAddVideoTile, (tileState) => {
       if (tileState.isScreenShare) {
-        this.setState(oldState => ({ 
-          ...oldState, 
-          screenShareTile: tileState.tileId
-        }));
+        setScreenShareTile(tileState.tileId)
       } else {
-        this.setState(oldState => ({ 
-          ...oldState, 
-          videoTiles: [...oldState.videoTiles, tileState.tileId],
-          selfVideoEnabled: tileState.isLocal ? true : oldState.selfVideoEnabled
-        }));
+        setVideoTiles(oldState => [...oldState.videoTiles, tileState.tileId])
+        setSelfVideoEnabled(oldState => tileState.isLocal ? true : oldState.selfVideoEnabled)
       }
-    });
+    }))
 
-    this.onRemoveVideoTileSubscription = getSDKEventEmitter().addListener(MobileSDKEvent.OnRemoveVideoTile, (tileState) => {
+    setOnRemoveVideoTileSubscription(getSDKEventEmitter().addListener(MobileSDKEvent.OnRemoveVideoTile, (tileState) => {
       if (tileState.isScreenShare) {
-        this.setState(oldState => ({ 
-          ...oldState, 
-          screenShareTile: null
-        }));
+        setScreenShareTile(null)
       } else {
-        this.setState(oldState => ({ 
-          ...oldState, 
-          videoTiles: oldState.videoTiles.filter(tileIdToCompare => tileIdToCompare != tileState.tileId),
-          selfVideoEnabled: tileState.isLocal ? false : oldState.selfVideoEnabled
-        }));
+        setVideoTiles(oldState => oldState.videoTiles.filter(tileIdToCompare => tileIdToCompare != tileState.tileId))
+        setSelfVideoEnabled(oldState => tileState.isLocal ? false : oldState.selfVideoEnabled)
       }
-
-    });
+    }))
 
     /**
      * General Error handler
      */
-    this.onErrorSubscription = getSDKEventEmitter().addListener(MobileSDKEvent.OnError, (errorType) => {
+    setOnErrorSubscription(getSDKEventEmitter().addListener(MobileSDKEvent.OnError, (errorType) => {
       switch(errorType) {
         case MeetingError.OnMaximumConcurrentVideoReached:
           Alert.alert("Failed to enable video", "maximum number of concurrent videos reached!");
-          break;
+        break;
         default:
           Alert.alert("Error", errorType);
-          break;
+        break;
       }
-    });
+    }))
   }
 
   componentWillUnmount() {
@@ -145,37 +119,37 @@ const Meeting = () => {
     const currentMuted = this.state.mutedAttendee.includes(this.props.selfAttendeeId);
     return (
       <View style={[styles.container, { justifyContent: 'flex-start' }]}>
-        <Text style={styles.title}>{this.props.meetingTitle}</Text>
-        <View style={styles.buttonContainer}>
-          <MuteButton muted={currentMuted} onPress={() => NativeFunction.setMute(!currentMuted) }/>         
-          <CameraButton disabled={this.state.selfVideoEnabled} onPress={() =>  NativeFunction.setCameraOn(!this.state.selfVideoEnabled)}/>
-          <HangOffButton onPress={() => NativeFunction.stopMeeting()} />
-        </View>
-        <Text style={styles.title}>Video</Text>
-        <View style={styles.videoContainer}>
-          {
-            this.state.videoTiles.length > 0 ? this.state.videoTiles.map(tileId => 
-              <RNVideoRenderView style={styles.video} key={tileId} tileId={tileId} />
-            ) : <Text style={styles.subtitle}>No one is sharing video at this moment</Text>
-          }
-        </View>
-        {
-          !!this.state.screenShareTile &&    
+      <Text style={styles.title}>{this.props.meetingTitle}</Text>
+      <View style={styles.buttonContainer}>
+      <MuteButton muted={currentMuted} onPress={() => NativeFunction.setMute(!currentMuted) }/>         
+      <CameraButton disabled={this.state.selfVideoEnabled} onPress={() =>  NativeFunction.setCameraOn(!this.state.selfVideoEnabled)}/>
+      <HangOffButton onPress={() => NativeFunction.stopMeeting()} />
+      </View>
+      <Text style={styles.title}>Video</Text>
+      <View style={styles.videoContainer}>
+      {
+        this.state.videoTiles.length > 0 ? this.state.videoTiles.map(tileId => 
+                                                                     <RNVideoRenderView style={styles.video} key={tileId} tileId={tileId} />
+                                                                    ) : <Text style={styles.subtitle}>No one is sharing video at this moment</Text>
+      }
+      </View>
+      {
+        !!this.state.screenShareTile &&    
           <React.Fragment>
             <Text style={styles.title}>Screen Share</Text>
             <View style={styles.videoContainer}>
               <RNVideoRenderView style={styles.screenShare} key={this.state.screenShareTile} tileId={this.state.screenShareTile} />
             </View>
           </React.Fragment>
-        }
-        <Text style={styles.title}>Attendee</Text>
+      }
+      <Text style={styles.title}>Attendee</Text>
         <FlatList
           style={styles.attendeeList}
           data={this.state.attendees}
           renderItem={({ item }) => <AttendeeItem attendeeName={attendeeNameMap[item] ? attendeeNameMap[item] : item} muted={this.state.mutedAttendee.includes(item)}/>}
           keyExtractor={(item) => item}
         />
-      </View>
+        </View>
     );
   }
 }
